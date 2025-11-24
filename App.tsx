2c7dbad92787ed4d2f1d06c657a8e340639ba3bb
AnaClara0813 <anamendes0813@gmail.com>
2025-11-24 15:13:18 -0300

    Fix app shell background to respect theme

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { ProvedorContextoApp } from './contexts/AppContext';
 import { ProvedorPlanoContas } from './contexts/PlanoContasContext';
 import { ProvedorLancamentos } from './contexts/LancamentosContext';
 import { AplicativoCondominio } from './components/AplicativoCondominio';
-import { Toaster } from './components/ui/sonner';
 
 export default function App() {
   return (
@@ -11,7 +10,6 @@ export default function App() {
         <ProvedorLancamentos>
           <div className="size-full">
             <AplicativoCondominio />
-             <Toaster richColors position="top-center" />
           </div>
         </ProvedorLancamentos>
       </ProvedorPlanoContas>
